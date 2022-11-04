2e2b81d8b03fe4d74d005ba341e7826e60c8a8d9
somho <110899057+somho@users.noreply.github.com>
2022-11-04 12:52:59 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,28 +1,27 @@
-$(function(){
+$(function () {
     // NAV
     var menu = $('.nav > li');
-    var contents = $('#wrap > div');  
+    var contents = $('#wrap > div');
 
-    $('.nav> li').click(function(event){
+    $('.nav> li').click(function (event) {
         event.preventDefault();
         var tg = $(this);
         var i = tg.index();
-                
+
         var section = $('#wrap > div').eq(i);
         var tt = section.offset().top;
-                
-        $('html, body').stop().animate({scrollTop:tt});
+
+        $('html, body').stop().animate({ scrollTop: tt });
     });
 
     $('.nav> li').on({
-        mouseover:function(){
+        mouseover: function () {
             $(this).find('a').addClass('on');
         },
-        mouseout:function(){
+        mouseout: function () {
             $('.nav>li').find('a').removeClass('on');
         }
 
-        
     });
 
     // M_TEXT
@@ -33,32 +32,112 @@ $(function(){
     let i = 0;
     let j = 0;
 
-    function typing(){
-        if (i<content.length) {
+    function typing() {
+        if (i < content.length) {
             let txt = content.charAt(i);
-            text.innerHTML += txt=== "\n" ? "<br/>": txt;
+            text.innerHTML += txt === "\n" ? "<br/>" : txt;
             i++;
         }
     }
 
-    function typing2(){
-        setTimeout(function(){
-            if (j<content2.length) {
+    function typing2() {
+        setTimeout(function () {
+            if (j < content2.length) {
                 let txt2 = content2.charAt(j);
                 text2.innerHTML += txt2;
                 j++;
             }
         }, 2000);
     }
-    setInterval(typing, 80);
-    setInterval(typing2, 40)
+    setInterval(typing, 70);
+    setInterval(typing2, 30)
+
+    // GRAPH
+    new Chart(document.getElementById("pie1"), {
+        type: 'doughnut',
+        data: {
+            // labels: ["HTML", "none"],
+            datasets: [{
+                backgroundColor: ["#3e95cd"],
+                data: [90, 10]
+            }]
+        }
+    });
+
+    new Chart(document.getElementById("pie2"), {
+        type: 'doughnut',
+        data: {
+            // labels: ["HTML", "none"],
+            datasets: [{
+                backgroundColor: ["#005aa7"],
+                data: [80, 20]
+            }]
+        }
+    });
+
+    new Chart(document.getElementById("pie3"), {
+        type: 'doughnut',
+        data: {
+            // labels: ["HTML", "none"],
+            datasets: [{
+                backgroundColor: ["#0AF781"],
+                data: [70, 30]
+            }]
+        }
+    });
+
+
+    new Chart(document.getElementById("pie4-1"), {
+        type: 'doughnut',
+        data: {
+            // labels: ["HTML", "none"],
+            datasets: [{
+                backgroundColor: ["#E0021C"],
+                data: [70, 30]
+            }]
+        }
+    });
+
+    new Chart(document.getElementById("pie4-2"), {
+        type: 'doughnut',
+        data: {
+            // labels: ["HTML", "none"],
+            datasets: [{
+                backgroundColor: ["#D30DE0"],
+                data: [70, 30]
+            }]
+        }
+    });
+
+    new Chart(document.getElementById("pie4-3"), {
+        type: 'doughnut',
+        data: {
+            // labels: ["HTML", "none"],
+            datasets: [{
+                backgroundColor: ["#8f94fb"],
+                data: [50, 50]
+            }]
+        }
+    });
+
+    new Chart(document.getElementById("pie4-4"), {
+        type: 'doughnut',
+        data: {
+            // labels: ["HTML", "none"],
+            datasets: [{
+                backgroundColor: ["#re54c8"],
+                data: [40, 60]
+            }]
+        }
+    });
+
 
 
     // QUICKMN
     $(window).scroll(
-        function(){
-        var	windowTop =$(window).scrollTop()+350;
-        $("#q_mn").stop().animate({top:windowTop+ "px" },300);
-            }
-        );
+        function () {
+            var windowTop = $(window).scrollTop() + 350;
+            $("#q_mn").stop().animate({ top: windowTop + "px" }, 300);
+        }
+    );
 });
